1549df1258941333526b817d4811624f6cca96e3
Rufus Raghunath <rraghun@thoughtworks.com>
2020-06-03 22:43:35 -0400

    Make check and select work for VirtualizedTable

--- client/src/tables/VirtualizedTable.tsx
+++ client/src/tables/VirtualizedTable.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
-import { Table, Column } from 'react-virtualized';
+import { Table, Column, TableCellProps } from 'react-virtualized';
 import 'react-virtualized/styles.css';
 import './Table.css';
 
 // https://github.com/bvaughn/react-virtualized/tree/master/docs#documentation
 
-function VirtualizedTable({ rows }: TableProps) {
+function VirtualizedTable({
+  rows,
+  checked,
+  selected,
+  check,
+  select,
+}: TableProps) {
+  function checkboxRenderer({ rowData }: TableCellProps) {
+    return (
+      <div>
+        <input
+          type="checkbox"
+          checked={checked === rowData.id}
+          onChange={(e) => {
+            check(rowData.id);
+          }}
+          onClick={(e) => e.stopPropagation()}
+        />{' '}
+      </div>
+    );
+  }
+
   return (
     <Table
-      height={500}
+      height={600}
       width={400}
       headerHeight={25}
       rowHeight={25}
       rowCount={rows.length}
       rowGetter={({ index }) => rows[index]}
       className="table"
-      rowClassName="row"
+      rowClassName={({ index }) =>
+        selected === rows[index]?.id ? 'row selected' : 'row'
+      }
+      onRowClick={({ rowData }) => select(rowData.id)}
     >
-      <Column label="" dataKey="checkbox" width={100} />
-      <Column label="ID" dataKey="id" width={100} />
-      <Column label="NAME" dataKey="name" width={100} />
-      <Column label="AGE" dataKey="age" width={100} />
+      <Column
+        label=""
+        dataKey="checkbox"
+        width={100}
+        headerClassName="header"
+        cellRenderer={checkboxRenderer}
+      />
+      <Column label="ID" dataKey="id" width={100} headerClassName="header" />
+      <Column
+        label="NAME"
+        dataKey="name"
+        width={100}
+        headerClassName="header"
+      />
+      <Column label="AGE" dataKey="age" width={100} headerClassName="header" />
     </Table>
   );
 }
